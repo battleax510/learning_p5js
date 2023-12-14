1331436d427e223d3c41ae211101decddef37cce
Chris Samuel <encryptable@pm.me>
2023-12-14 10:40:53 -0500

    addiing scripts folder for handling tools

---
+++ projects/scripts/trim_audio.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if FFmpeg is installed
+if ! command -v ffmpeg &> /dev/null; then
+    echo "FFmpeg is not installed. Please install FFmpeg and try again."
+    exit 1
+fi
+
+# Check if input file is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 1
+fi
+
+# Check if output file is provided
+if [ -z "$2" ]; then
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 1
+fi
+
+input_file="$1"
+output_file="$2"
+
+# Trim the audio file
+ffmpeg -i "$input_file" -ss 0 -t 30 -acodec copy "$output_file"
+
+echo "Trimmed audio file created: $output_file"
