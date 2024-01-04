9022c925914750af5d5c27d36e4f0bbf9ec23a53
Chris Samuel <encryptable@pm.me>
2024-01-04 11:41:19 -0500

    added files for functions

---
+++ projects/memory-game/cardFunctions.js
@@ -0,0 +1,11 @@
+function shuffleCards() {
+    // Implementation
+  }
+  
+  function generateCards() {
+    // Implementation
+  }
+  
+  function createCardElement(card) {
+    // Implementation
+  }
