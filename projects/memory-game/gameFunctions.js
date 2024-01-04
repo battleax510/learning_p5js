13bfa14cc988d3c4feaea632d8a86d85c15b6fe7
Chris Samuel <encryptable@pm.me>
2024-01-04 11:41:19 -0500

    added files for functions

---
+++ projects/memory-game/gameFunctions.js
@@ -0,0 +1,19 @@
+function flipCard() {
+    // Implementation
+  }
+  
+  function checkForMatch() {
+    // Implementation
+  }
+  
+  function disableCards() {
+    // Implementation
+  }
+  
+  function unflipCards() {
+    // Implementation
+  }
+  
+  function resetBoard() {
+    // Implementation
+  }
