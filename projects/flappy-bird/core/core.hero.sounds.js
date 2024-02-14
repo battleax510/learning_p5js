ae93b8e4e2e919c5462c7facdc0d2f83e5a34ee4
battleax510 <150942356+battleax510@users.noreply.github.com>
2024-02-14 10:43:35 -0500

    added more of the sounds to the game

--- projects/flappy-bird/core/core.hero.sounds.js
+++ projects/flappy-bird/core/core.hero.sounds.js
@@ -4,7 +4,10 @@ export const heroPerishSounds =  {
     perish_two: "sounds_effect/hero-perish-02.wav",
     perish_three: "sounds_effect/hero-perish-03.wav",
     perish_four: "sounds_effect/hero-perish-04.wav",
-    perish_five: "sounds_effect/hero-perish-05.wav"
+    perish_five: "sounds_effect/hero-perish-05.wav",
+    perish_six: "sounds_effect/hero-perish-06.wav",
+    perish_eight: "sounds_effect/hero-perish-08.wav"
+    
 } 
 
 export const heroJumpSounds =  {
@@ -13,5 +16,10 @@ export const heroJumpSounds =  {
     jump_two: "sounds_effect/hero-jump-02.wav",
     jump_three: "sounds_effect/hero-jump-03.wav",
     jump_four: "sounds_effect/hero-jump-04.wav",
-    jump_five: "sounds_effect/hero-jump-05.wav"
+    jump_five: "sounds_effect/hero-jump-05.wav",
+    jump_six: "sounds_effect/hero-jump-06.wav",
+    jump_eight: "sounds_effect/hero-jump-08.wav",
+    jump_ten: "sounds_effect/hero-jump-10.wav",
+    jump_eleven: "sounds_effect/hero-jump-11.wav"
+    
 } 
