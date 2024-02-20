68a9cd7fd8bc5b9f7b777f505e3ad4a8f47a879f
Chris Samuel <encryptable@pm.me>
2024-02-20 11:23:19 -0500

    added

---
+++ projects/flappy-bird/core/core.ghost.sounds.js
@@ -0,0 +1,18 @@
+
+export const ghostPerishSounds =  {
+    perish_zero: "sounds_effect/ghostSounds/ghost-undeath.wav"
+}
+
+
+export const ghostJumpSounds =  {
+    jump_zero: "sounds_effect/hero-jump-00.wav",
+    jump_one: "sounds_effect/hero-jump-01.wav",
+    jump_two: "sounds_effect/hero-jump-02.wav",
+    jump_three: "sounds_effect/hero-jump-03.wav",
+    jump_four: "sounds_effect/hero-jump-04.wav",
+    jump_five: "sounds_effect/hero-jump-05.wav",
+} 
+
+
+
+
