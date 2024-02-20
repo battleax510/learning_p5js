7d9c5180633cf04049ed09b55aeb255368204ed0
battleax510 <150942356+battleax510@users.noreply.github.com>
2024-02-20 11:33:00 -0500

    added ghost jump sounds

--- projects/flappy-bird/core/core.ghost,sounds.js
+++ projects/flappy-bird/core/core.ghost,sounds.js
@@ -5,12 +5,11 @@ export const ghostPerishSounds =  {
 
 
 export const ghostJumpSounds =  {
-    jump_zero: "sounds_effect/hero-jump-00.wav",
-    jump_one: "sounds_effect/hero-jump-01.wav",
-    jump_two: "sounds_effect/hero-jump-02.wav",
-    jump_three: "sounds_effect/hero-jump-03.wav",
-    jump_four: "sounds_effect/hero-jump-04.wav",
-    jump_five: "sounds_effect/hero-jump-05.wav",
+    jump_zero: "sounds_effect/ghostSounds/ghost-chuckle.wav",
+    jump_one: "sounds_effect/ghostSounds/ghost-giggle.wav",
+    jump_two: "sounds_effect/ghostSounds/ghost-laugh.wav",
+    jump_three: "sounds_effect/ghostSounds/ghost-roar.wav",
+    jump_four: "sounds_effect/ghostSounds/ghost-snicker.wav",
 } 
 
 
