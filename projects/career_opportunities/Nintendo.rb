84a10b93dc212423a6247103be6afc7d63ff5854
battleax510 <150942356+battleax510@users.noreply.github.com>
2024-03-07 10:56:54 -0500

    jobs

---
+++ projects/career_opportunities/Nintendo.rb
@@ -0,0 +1,17 @@
+Nintendo
+https://careers.nintendo.com/?_gl=1*yapu1b*_ga*MTA4NTUwNzU0NS4xNzA5ODI2Mjgx*_ga_F6ERC4HMNZ*MTcwOTgyNjI4MS4xLjEuMTcwOTgyNjM1NS4wLjAuMA..
+https://www.youtube.com/nintendo
+https://twitter.com/NintendoAmerica
+https://www.facebook.com/NintendoAmerica/
+https://www.instagram.com/nintendoamerica/
+
+pikmin 1, 2, & 3
+splatoon 2 & 3
+lots of mario games
+kirby games
+
+
+first consol DS
+new super mario bros
+mario cart ds
+pokemon black 2
