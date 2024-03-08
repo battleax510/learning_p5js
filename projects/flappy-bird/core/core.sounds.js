67a8f5b0bcf50c2c782b95709fd05ac562fa6b8c
Chris Samuel <encryptable@pm.me>
2024-03-08 10:37:38 -0500

    adding core and dist

---
+++ projects/flappy-bird/core/core.sounds.js
@@ -0,0 +1,8 @@
+// In a separate file, let's call it soundPaths.js
+export const soundPaths = {
+    sound_one: "sounds_effect/pvz.super.brainz.voice.clips.IGE-Ht-rLJk.wav-037.wav",
+    sound_two: "sounds_effect/pvz.super.brainz.voice.clips.IGE-Ht-rLJk.wav-004.wav",
+    sound_three: "sounds_effect/pvz.super.brainz.voice.clips.IGE-Ht-rLJk.wav-028.wav",
+    sound_four: "sounds_effect/pvz.super.brainz.voice.clips.IGE-Ht-rLJk.wav-081.wav",
+    sound_five: "sounds_effect/pvz.super.brainz.voice.clips.IGE-Ht-rLJk.wav-018.wav",
+};
